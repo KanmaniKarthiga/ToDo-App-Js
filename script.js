8865b5dc7023453ba165167ad43b0375515c2470
kanmanikarthiga <karthiga.selva98@gmail.com>
2023-06-26 00:54:55 +0530

    changes

--- script.js
+++ script.js
@@ -1,57 +1,139 @@
-const section = document.querySelector('section')
-let overlay = document.querySelector('.overlay')
-let addicon = document.querySelector('#iconplus')
-let listbtn = document.getElementById('list')
-let addbtn  = document.querySelector('#add')
-let closebtn = document.querySelector('#close')
+var blurdiv = document.querySelector('div')
+var addicon = document.querySelector('#iconplus')
+var listbtn = document.getElementById('list')
+var addbtn  = document.querySelector('#add')
+var closebtn = document.querySelector('#close')
+var navbar = document.querySelector('nav')
+var section = document.querySelector('section')
+var add = document.getElementById('addtask')
+var item = document.getElementById('additem')
+var item_addbtn = document.querySelector('#add_itemBtn')
+var item_closebtn = document.querySelector('#close_itemBtn')
+var itemText = document.getElementById('iteminlist')
+var uniqueAdd = document.querySelector('#iconplus2')
+var uniqueClose = document.querySelector('.textofback')
+var backicon = document.querySelector('#backicon')
+console.log(itemText)
 
-let add = document.getElementById('addtask')
+
 function addtask(){
     add.style.display = "block"
+    blurdiv.classList.add('blur')
 }
 function hide(){
     add.style.display = "none"
+    blurdiv.classList.remove('blur')
+
 }
 
-
 var todoCount = 0
-var todos = document.getElementById('todos')
-function createPopup(){
+function createtasks(){
     todoCount++;
+
+    blurdiv.classList.remove('blur')
+	add.style.display = 'none'
+	document.querySelector('.header2').style.display = 'none'
 
     var newTodo = document.createElement("div")
     var TodoHeading = document.createElement("h1")
-    var todoDetail = document.createElement("p")
-    var delTodo = document.createElement("button")
+    var line = document.createElement("hr")
+    var delTodo = document.createElement("div")
+    var addTodo = document.createElement("div")
 
-    todos.appendChild(newTodo)
-    newTodo.appendChild(TodoHeading)
-    newTodo.appendChild(todoDetail)
-    newTodo.appendChild(delTodo)
+    section.appendChild(newTodo)
+    newTodo.setAttribute('id', `newTodo${todoCount}`)
+	newTodo.classList.add('newTodo')
 
-    TodoHeading.innerText = list.value
-    todoDetail.innerText = "Task description"
-    newTodo.classList.add("todocard")
-    delTodo.innerText = "X"
-    delTodo.style.backgroundColor = "red"
-    // addItem.innerText = 
+	newTodo.appendChild(TodoHeading)
+	TodoHeading.classList.add('TodoHeading')
+	TodoHeading.innerHTML = `${listbtn.value}`
+
+	newTodo.appendChild(line)
+	newTodo.appendChild(delTodo)
+	delTodo.classList.add('delTodo')
+	delTodo.innerHTML = `X`
+	delTodo.addEventListener('click', () => {
+		var delCard = delTodo.parentNode
+		delCard.remove()
+	})
+
+	newTodo.appendChild(addTodo)
+	addTodo.classList.add('addTodo')
+	addTodo.innerHTML = `+`
+	
+	addTodo.addEventListener('click', () => {
+		blurdiv.classList.add('blur')
+		item.style.display = "block"
+	})
+
+	TodoHeading.addEventListener('click', () =>{
+	newTodo.style.marginLeft = "400px"
+
+	var newHeader = document.createElement('div')
+	section.appendChild(newHeader)
+	newHeader.setAttribute('id', `newheader`)
+	newHeader.innerHTML = listbtn.value
+	newHeader.style.display = 'block'
+	navbar.style.display = 'none'
+	document.querySelector('.unique').style.display = 'flex'
+
+	uniqueAdd.addEventListener('click',() =>{
+		addtask()
+		document.querySelector('.unique').style.display = 'none'
+		newHeader.style.display = 'none'
+		section.style.display = 'flex'
+		navbar.style.display = 'block'
+		newTodo.style.margin = "2px"
+		
+	})
+	uniqueClose.addEventListener('click', () =>{
+		document.querySelector('.unique').style.display = 'none'
+		newHeader.style.display = 'none'
+		section.style.display = 'flex'
+		navbar.style.display = 'block'
+	})
+	backicon.addEventListener('click', () =>{
+		document.querySelector('.unique').style.display = 'none'
+		newHeader.style.display = 'none'
+		section.style.display = 'flex'
+		navbar.style.display = 'block'
+		newTodo.style.margin = "2px"
+	})
 
 
+})	
+	var itemCount = 0
+	item_closebtn.addEventListener('click', () =>{
+		item.style.display = 'none'
+		blurdiv.classList.remove('blur')
+	})
+	item_addbtn.addEventListener('click', () =>{
+		itemCount++;
+		blurdiv.classList.remove('blur')
+		item.style.display = 'none'
+
+		var newparent = document.createElement('div')
+		var newItem = document.createElement('div')
+		var statusbar = document.createElement('div')
+		
+		newTodo.appendChild(newparent)
+		newparent.appendChild(newItem)
+		newparent.appendChild(statusbar)
+
+		newItem.setAttribute('id', `newItem${itemCount}`)
+		newparent.classList.add('newItem')
+		newItem.innerHTML = itemText.value
+		
+		statusbar.classList.add('statusbar')
+		statusbar.innerHTML = "Mark Done"	
+		statusbar.addEventListener('click', () =>{
+			statusbar.style.display = 'none'
+			newItem.style.textDecoration = "line-through"
+			newItem.style.color = "blue"
+		})
+	})
+	
 }
 
-// var popup = document.getElementById('popup')
-// var isOpen = false
-// function show(){
-//     if(isOpen){
-//         popup.classList.remove("show")
-//         isOpen = false
-//     }
-//     else{
-//         popup.classList.add("show")
-//         isOpen = true
-//     }
-// }
-// function hide(){
-//     popup.classList.remove("show")
-// }
+	
 
